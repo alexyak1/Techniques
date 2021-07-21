3b3407d8a33f9d6ac041bdd8dc5ee2e05b30a5ca
ekmclks <aleksandr.iakimchuk@ericsson.com>
2021-07-21 21:37:07 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+       fmt.Fprintf(w, "Here you will see judo techniques, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "First techniq /n Second techniq")
+    })
+
+    log.Fatal(http.ListenAndServe(":8787", nil))
+}
