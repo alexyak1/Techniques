3505b00f342c6f9c65fe66b6fcf456a1e1569b52
alexyak1 <yakimchuk.nstu@gmail.com>
2021-07-23 08:34:46 +0200

    autodeploy

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t techniques .
-docker run -p 8787:8787 -d techniques
+docker rm -f techniques-container || true
+docker run -p 8787:8787 -d --name techniques-container techniques
