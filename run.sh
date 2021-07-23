9b9836ff21f84aecd505fdbf2aee170218f5bf9e
alexyak1 <yakimchuk.nstu@gmail.com>
2021-07-23 18:02:37 +0200

    test

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-pwd
-git reset --hard origin/master
-git pull origin master
-docker build -t techniques .
-docker rm -f techniques-container || true
-docker run -p 8787:8787 -d --name techniques-container techniques
+
