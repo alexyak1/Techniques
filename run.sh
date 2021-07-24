7c55324bcaf9df984142bd59b1826a9221b4c557
alexyak1 <yakimchuk.nstu@gmail.com>
2021-07-24 12:14:40 +0200

    try to get last changes

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-pwd
-git reset --hard origin/master
-git pull origin master
+cd /home/ubuntu/projects/Techniques
 docker build -t techniques .
 docker rm -f techniques-container || true
 docker run -p 8787:8787 -d --name techniques-container techniques
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pwd
+cd /home/ubuntu/projects/Techniques
+git reset --hard origin/master
+git pull origin master
