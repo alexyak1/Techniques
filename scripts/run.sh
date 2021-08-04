1dec12b6d95fbe510a47e6b8b4a605494f63e7ad
alexyak1 <yakimchuk.nstu@gmail.com>
2021-08-04 22:27:21 +0200

    config deploy

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/projects/Techniques
 
+git reset --hard origin/master
 git pull origin master
+sudo chmod 777 -R /home/ubuntu/projects/Techniques/mysql
 
 docker-compose build
 docker-compose up -d
