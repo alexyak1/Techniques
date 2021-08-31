afe107399c7b0ecccf6a11e1bbe46b2d1d4af01d
alexyak1 <yakimchuk.nstu@gmail.com>
2021-08-31 14:49:03 +0200

    add type for technique

--- entity/technique.go
+++ entity/technique.go
@@ -1,8 +1,9 @@
 package entity
 
 type Technique struct {
-	Id      string    `json:"id"`
-    Name   string `json:"name"`
+	Id string `json:"id"`
+    Name string `json:"name"`
     Belt string `json:"belt"`
     ImageURL string `json:"image_url"`
+    Type string `json: "type"`
 }
