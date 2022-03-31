627109585f15bb2dbde1736a0513ef8c4b1548eb
alexyak1 <yakimchuk.nstu@gmail.com>
2022-03-31 15:20:03 +0200

    trying to use dynamic port

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"main/database"
 	"main/entity"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -39,6 +40,12 @@ func initDB() {
 }
 
 func handleRequests() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/technique", controllers.CreateTechnique).Methods("POST")
 	myRouter.HandleFunc("/technique/{id}", controllers.DeleteTechniqueById).Methods("DELETE")
@@ -46,12 +53,17 @@ func handleRequests() {
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/techniques", controllers.GetAllTechniques)
 	myRouter.HandleFunc("/technique/{id}", controllers.GetTechniqueById)
-	log.Fatal(http.ListenAndServe(":80", myRouter))
+	log.Fatal(http.ListenAndServe(":"+port, myRouter))
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	fmt.Fprintf(w, "Hi judoka!\nWelcome to the HomePage of judo tecniques! ")
-	fmt.Fprintf(w, "\nFor get all techniques visit this endpoint:\n http://18.221.140.18:80/techniques")
+	fmt.Fprintf(w, "\nFor get all techniques visit this endpoint:\n http://18.221.140.18:"+port+"/techniques")
 
 	fmt.Println("Endpoint Hit: homePage")
 }
