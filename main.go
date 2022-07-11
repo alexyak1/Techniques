81a83636124dc2b8b86897c509e82c56f3a8cf34
ekmclks <aleksandr.iakimchuk@ericsson.com>
2022-07-11 13:07:30 +0200

    server name changed right

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func initDB() {
 		// 	}
 		config =
 			database.Config{
-				ServerName: "db4free.net/",
+				ServerName: "db4free.net",
 				User:       "notrootuser",
 				Hash:       db_password,
 				DB:         "techniques",
