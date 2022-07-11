93594cce96da913088c579b4228d92ca01134e05
alexyak1 <yakimchuk.nstu@gmail.com>
2022-07-11 10:32:56 +0200

    use heroku db as documented

--- database/client.go
+++ database/client.go
@@ -1,13 +1,15 @@
 package database
 
 import (
+	"database/sql"
 	"log"
 	"main/entity"
+	"os"
 
 	// "github.com/jinzhu/gorm"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
 
 var Connector *gorm.DB
@@ -15,12 +17,9 @@ var Connector *gorm.DB
 func Connect(connectionString string) error {
 	var err error
 	Connector, err = gorm.Open("mysql", connectionString)
-	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "host=ec2-34-248-169-69.eu-west-1.compute.amazonaws.com user=llxgckjvfqvhuw password=0d63932135f5c6a82950d2423b763fbc7308a12555ac31154ea968ebb86ac3ce dbname=d6juhn5dera7pp port=5432 sslmode=disable TimeZone=Asia/Shanghai", // data source name, refer https://github.com/jackc/pgx
-		PreferSimpleProtocol: true,                                                                                                                                                                                                                          // disables implicit prepared statement usage. By default pgx automatically uses the extended protocol
-	}), &gorm.Config{})
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 
 	log.Println("Connection success")
