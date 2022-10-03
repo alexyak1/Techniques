4d5755cf99f9b12189e749236e7c2d95a645c1b8
alexyak1 <yakimchuk.nstu@gmail.com>
2022-10-03 10:17:54 +0200

    add image_id to technique

--- entity/technique.go
+++ entity/technique.go
@@ -1,9 +1,10 @@
 package entity
 
 type Technique struct {
-	Id *string `json:"id"`
-    Name string `json:"name"`
-    Belt string `json:"belt"`
-    ImageURL string `json:"image_url"`
-    Type string `json:"type"`
+	Id       *string `json:"id"`
+	Name     string  `json:"name"`
+	Belt     string  `json:"belt"`
+	ImageURL string  `json:"image_url"`
+	ImageId  string  `json:"image_id"`
+	Type     string  `json:"type"`
 }
