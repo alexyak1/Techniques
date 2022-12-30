01175a5374a85394f1ef5d769d38e0df10ff9a55
Aleksandr Iakimchuk <yakimchuk.nstu@gmail.com>
2022-12-30 20:54:28 +0100

    blog endpoint with join

--- controllers/blogItems.go
+++ controllers/blogItems.go
@@ -17,3 +17,19 @@ func GetBlogData(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(blogItems)
 }
+
+func GetBlogDataRawQuery(w http.ResponseWriter, r *http.Request) {
+	var blogItems []entity.BlogItem
+
+	// only blog_items
+	// database.Connector.Find(&blogItems)
+
+	// TODO: was bad connection need to test with join
+	// With join to pool answers
+	database.Connector.Joins("JOIN blog_pool_answers on blog_pool_answers.blog_item_id = blog_items.id").Find(&blogItems)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(blogItems)
+}
